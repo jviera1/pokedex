55d80636fcb34cc59076da7aa38ba4c47952c753
jviera1 <jviera1@asu.edu>
2021-07-10 20:49:40 -0700

    Saying Hello World!
    Intro to React

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+            Hello world! My name is Joshua
         </p>
+        <p> ;)</p>
         <a
           className="App-link"
           href="https://reactjs.org"
